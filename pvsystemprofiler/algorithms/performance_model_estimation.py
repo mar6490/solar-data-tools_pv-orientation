0671216e2201e53beb81c99c4c998796c0ff5772
mar6490 <141142611+mar6490@users.noreply.github.com>
2026-01-19 14:27:56 +0100

    Add notebook to test orientation on local CSV

--- pvsystemprofiler/algorithms/performance_model_estimation.py
+++ pvsystemprofiler/algorithms/performance_model_estimation.py
@@ -33,11 +33,20 @@ def find_fit_costheta(data_matrix, clear_index, doy):
     phi2 = 0
     z = cvx.Variable()
     constraints.append(cvx.diff(x2, k=1) == z)
-    phi3 = 1e3 * cvx.sum_squares(cvx.diff(x3, k=2)) + 1e-6 * cvx.norm1(x3)
-    for val in set(solstice_centered_index(doy)):
-        constraints.append(cvx.diff(x3[solstice_centered_index(doy) == val]) == 0)
+    if len(data) > 2:
+        phi3 = 1e3 * cvx.sum_squares(cvx.diff(x3, k=2)) + 1e-6 * cvx.norm1(x3)
+    else:
+        phi3 = 1e-6 * cvx.norm1(x3)
+    solstice_index = solstice_centered_index(doy)
+    for val in set(solstice_index):
+        day_mask = solstice_index == val
+        if np.sum(day_mask) > 2:
+            constraints.append(cvx.diff(x3[day_mask]) == 0)
     constraints.append(x3 <= 0)
-    phi4 = 1e3 * cvx.sum_squares(cvx.diff(x4, k=2)) + 1e-3 * cvx.sum_squares(x4)
+    if len(data) > 2:
+        phi4 = 1e3 * cvx.sum_squares(cvx.diff(x4, k=2)) + 1e-3 * cvx.sum_squares(x4)
+    else:
+        phi4 = 1e-3 * cvx.sum_squares(x4)
     constraints.append(x4[:-365] == x4[365:])
     constraints.append(x4 <= 0)
     objective = phi1 + phi2 + phi3 + phi4
